c9415a1ad78f2f2234397bb590acd2e4913db7ad
Steve <stevegardiner26@aol.com>
2024-12-04 17:10:32 -0500

    Simplify part 1

--- day4/part1.rb
+++ day4/part1.rb
@@ -1,82 +1,51 @@
-input = File.read('input.txt')
+input = File.read('input.txt') # 85 lines
 
-mapping_array = input.split(/\n/)
-mapping_array.map!{|a| a.split('')}
+mapping_array = input.split(/\n/).map{|a| a.split('')}
 xmas_count = 0
 
 mapping_array.each_with_index do |row, y|
     row.each_with_index do |item, x|
         next if item != "X"
-        # We need to check all directions for an "M" and 
-        # TOP LEFT
-        if mapping_array[y-1] && mapping_array[y-1][x-1] == "M" && (y-3 >= 0) && (x-3 >= 0)
-            if mapping_array[y-2] && mapping_array[y-2][x-2] == "A"
-                if mapping_array[y-3] && mapping_array[y-3][x-3] == "S"
-                    xmas_count += 1
-                end
-            end
-        end
-
-        # TOP MIDDLE
-        if mapping_array[y-1] && mapping_array[y-1][x] == "M" && (y - 3 >= 0)
-            if mapping_array[y-2] && mapping_array[y-2][x] == "A"
-                if mapping_array[y-3] && mapping_array[y-3][x] == "S"
-                    xmas_count += 1
-                end
-            end
-        end
-
-        # TOP RIGHT
-        if mapping_array[y-1] && mapping_array[y-1][x+1] == "M" && (y - 3 >= 0)
-            if mapping_array[y-2] && mapping_array[y-2][x+2] == "A"
-                if mapping_array[y-3] && mapping_array[y-3][x+3] == "S"
-                    xmas_count += 1
-                end
-            end
-        end
-
-        # LEFT
-        if mapping_array[y][x-1] == "M" && (x-3 >= 0)
-            if mapping_array[y][x-2] == "A"
-                if mapping_array[y][x-3] == "S"
-                    xmas_count += 1
-                end
-            end
-        end
-
-        # RIGHT
-        if mapping_array[y][x+1] == "M"
-            if mapping_array[y][x+2] == "A"
-                if mapping_array[y][x+3] == "S"
-                    xmas_count += 1
-                end
-            end
-        end
-
-        # BOTTOM LEFT
-        if mapping_array[y+1] && mapping_array[y+1][x-1] == "M" && (x-3 >= 0)
-            if mapping_array[y+2] && mapping_array[y+2][x-2] == "A"
-                if mapping_array[y+3] && mapping_array[y+3][x-3] == "S"
-                    xmas_count += 1
-                end
-            end
-        end
         
-        # BOTTOM MIDDLE
-        if mapping_array[y+1] && mapping_array[y+1][x] == "M"
-            if mapping_array[y+2] && mapping_array[y+2][x] == "A"
-                if mapping_array[y+3][x] == "S"
-                    xmas_count += 1
-                end
+        if y-3 >= 0 && x-3 >= 0
+            # TOP LEFT
+            if mapping_array[y-1][x-1] == "M" && mapping_array[y-2][x-2] == "A" && mapping_array[y-3][x-3] == "S"
+                xmas_count += 1
             end
-        end
 
-        # BOTTOM RIGHT
-        if mapping_array[y+1] && mapping_array[y+1][x+1] == "M"
-            if mapping_array[y+2] && mapping_array[y+2][x+2] == "A"
-                if mapping_array[y+3] && mapping_array[y+3][x+3] == "S"
-                    xmas_count += 1
-                end
+            # TOP MIDDLE
+            if mapping_array[y-1][x] == "M" && mapping_array[y-2][x] == "A" && mapping_array[y-3] && mapping_array[y-3][x] == "S"
+                xmas_count += 1
+            end
+
+            # TOP RIGHT
+            if mapping_array[y-1][x+1] == "M" && mapping_array[y-2][x+2] == "A" && mapping_array[y-3][x+3] == "S"
+                xmas_count += 1
+            end
+            
+            # LEFT
+            if mapping_array[y][x-1] == "M" && mapping_array[y][x-2] == "A" && mapping_array[y][x-3] == "S"
+                xmas_count += 1
+            end
+
+            # RIGHT
+            if mapping_array[y][x+1] == "M" && mapping_array[y][x+2] == "A" && mapping_array[y][x+3] == "S"
+                xmas_count += 1
+            end
+
+            # BOTTOM LEFT
+            if mapping_array[y+1][x-1] == "M" && mapping_array[y+2][x-2] == "A" && mapping_array[y+3][x-3] == "S"
+                xmas_count += 1
+            end
+        
+            # BOTTOM MIDDLE
+            if mapping_array[y+1][x] == "M" && mapping_array[y+2][x] == "A" && mapping_array[y+3][x] == "S"
+                xmas_count += 1
+            end
+
+            # BOTTOM RIGHT
+            if mapping_array[y+1][x+1] == "M" && mapping_array[y+2][x+2] == "A" && mapping_array[y+3][x+3] == "S"
+                xmas_count += 1
             end
         end
     end
