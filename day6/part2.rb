47e31b0af63d5e14fbadd7f83dfecec8c686ad5d
Steve <stevegardiner26@aol.com>
2024-12-06 23:38:43 -0500

    Part 2 Finally

--- day6/part2.rb
+++ day6/part2.rb
@@ -13,82 +13,90 @@ initial_grid.each_with_index do |y, y_idx|
     end
 end
 
-def move(grid, current_position, visited_array)
-    current_value = grid[current_position[0]][current_position[1]]
-    if current_value == '^'
-        return false if (current_position[0] - 1) < 0
-        next_value = grid[current_position[0] - 1][current_position[1]]
-
-        if next_value == '#'
-            grid[current_position[0]][current_position[1]] = '>'
-            arr_key = "#{current_position[0]},#{current_position[1]}"
-            return 1 if visited_array[arr_key] && visited_array[arr_key] > 1
-            visited_array[arr_key] ? visited_array[arr_key] += 1 : visited_array[arr_key] = 0
-        else
-            grid[current_position[0]][current_position[1]] = 'X'
-            grid[current_position[0] - 1][current_position[1]] = current_value
-            current_position = [current_position[0] - 1, current_position[1]]
-        end
-    elsif current_value == '>'
-        return false if current_position[1] + 1 > grid[0].length
-        next_value = grid[current_position[0]][current_position[1] + 1]
-
-        if next_value == '#'
-            grid[current_position[0]][current_position[1]] = 'V'
-            arr_key = "#{current_position[0]},#{current_position[1]}"
-            return 1 if visited_array[arr_key] && visited_array[arr_key] > 1
-            visited_array[arr_key] ? visited_array[arr_key] += 1 : visited_array[arr_key] = 0
-        else
-            grid[current_position[0]][current_position[1]] = 'X'
-            grid[current_position[0]][current_position[1] + 1] = current_value
-            current_position = [current_position[0], current_position[1] + 1]
-        end
-    elsif current_value == 'V'
-        return false if (current_position[0] + 1) >= grid.length
-        next_value = grid[current_position[0] + 1][current_position[1]]
-
-        if next_value == '#'
-            grid[current_position[0]][current_position[1]] = '<'
-            arr_key = "#{current_position[0]},#{current_position[1]}"
-            return 1 if visited_array[arr_key] && visited_array[arr_key] > 1
-            visited_array[arr_key] ? visited_array[arr_key] += 1 : visited_array[arr_key] = 0
-        else
-            grid[current_position[0]][current_position[1]] = 'X'
-            grid[current_position[0] + 1][current_position[1]] = current_value
-            current_position = [current_position[0] + 1, current_position[1]]
-        end
-    elsif current_value == '<'
-        return false if (current_position[1] - 1) < 0
-        next_value = grid[current_position[0]][current_position[1] - 1]
-
-        if next_value == '#'
-            grid[current_position[0]][current_position[1]] = '^'
-            arr_key = "#{current_position[0]},#{current_position[1]}"
-            return 1 if visited_array[arr_key] && visited_array[arr_key] > 1
-            visited_array[arr_key] ? visited_array[arr_key] += 1 : visited_array[arr_key] = 0
-        else
-            grid[current_position[0]][current_position[1]] = 'X'
-            grid[current_position[0]][current_position[1] - 1] = current_value
-            current_position = [current_position[0], current_position[1] - 1]
-        end
-    end
-
-    move(grid, current_position, visited_array)
-end
-
 # Go through every position in the grid from initial position Y, starting at X 0 and replace each one 1 by 1 with a #
 # Make a new grid var and pass it into the move. Check in the move function if it hits that specific coordinate as the "next item" more than once.
 # break the recursion and add to the total count
 
 initial_grid.each_with_index do |y, y_idx|
-    if y_idx >= initial_position[0]
-        puts "Y INDEX #{y_idx}"
+    if y_idx >= initial_position[0] || true
         y.each_with_index do |x, x_idx|
             next if x_idx == initial_position[1] && y_idx == initial_position[0]
-            modified_grid = initial_grid.map(&:dup)
-            modified_grid[y_idx][x_idx] = '#'
-            result = move(modified_grid, initial_position, {})
-            final_obstacle_count += 1 if result == 1
+            grid = initial_grid.map(&:dup)
+            grid[y_idx][x_idx] = '#'
+            current_position = initial_position.dup
+            visited_array = {}
+            while true do
+                current_value = grid[current_position[0]][current_position[1]]
+                if current_value == '^'
+                    break if (current_position[0] - 1) < 0
+                    next_value = grid[current_position[0] - 1][current_position[1]]
+
+                    if next_value == '#'
+                        grid[current_position[0]][current_position[1]] = '>'
+                        arr_key = "#{current_position[0]},#{current_position[1]}"
+                        if visited_array[arr_key] && visited_array[arr_key] >= 1
+                            final_obstacle_count += 1
+                            break
+                        end
+                        visited_array[arr_key] ? visited_array[arr_key] += 1 : visited_array[arr_key] = 0
+                    else
+                        grid[current_position[0]][current_position[1]] = 'X'
+                        grid[current_position[0] - 1][current_position[1]] = current_value
+                        current_position = [current_position[0] - 1, current_position[1]]
+                    end
+                elsif current_value == '>'
+                    break if current_position[1] + 1 > grid[0].length
+                    next_value = grid[current_position[0]][current_position[1] + 1]
+
+                    if next_value == '#'
+                        grid[current_position[0]][current_position[1]] = 'V'
+                        arr_key = "#{current_position[0]},#{current_position[1]}"
+                        if visited_array[arr_key] && visited_array[arr_key] >= 1
+                            final_obstacle_count += 1
+                            break
+                        end
+                        visited_array[arr_key] ? visited_array[arr_key] += 1 : visited_array[arr_key] = 0
+                    else
+                        grid[current_position[0]][current_position[1]] = 'X'
+                        grid[current_position[0]][current_position[1] + 1] = current_value
+                        current_position = [current_position[0], current_position[1] + 1]
+                    end
+                elsif current_value == 'V'
+                    break if (current_position[0] + 1) >= grid.length
+                    next_value = grid[current_position[0] + 1][current_position[1]]
+
+                    if next_value == '#'
+                        grid[current_position[0]][current_position[1]] = '<'
+                        arr_key = "#{current_position[0]},#{current_position[1]}"
+                        if visited_array[arr_key] && visited_array[arr_key] >= 1
+                            final_obstacle_count += 1
+                            break
+                        end
+                        visited_array[arr_key] ? visited_array[arr_key] += 1 : visited_array[arr_key] = 0
+                    else
+                        grid[current_position[0]][current_position[1]] = 'X'
+                        grid[current_position[0] + 1][current_position[1]] = current_value
+                        current_position = [current_position[0] + 1, current_position[1]]
+                    end
+                elsif current_value == '<'
+                    break if (current_position[1] - 1) < 0
+                    next_value = grid[current_position[0]][current_position[1] - 1]
+
+                    if next_value == '#'
+                        grid[current_position[0]][current_position[1]] = '^'
+                        arr_key = "#{current_position[0]},#{current_position[1]}"
+                        if visited_array[arr_key] && visited_array[arr_key] >= 1
+                            final_obstacle_count += 1
+                            break
+                        end
+                        visited_array[arr_key] ? visited_array[arr_key] += 1 : visited_array[arr_key] = 0
+                    else
+                        grid[current_position[0]][current_position[1]] = 'X'
+                        grid[current_position[0]][current_position[1] - 1] = current_value
+                        current_position = [current_position[0], current_position[1] - 1]
+                    end
+                end
+            end
         end
     end
 end
